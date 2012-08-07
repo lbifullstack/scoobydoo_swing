c34b12c0e0a9c5f6cb67eafa663352c84c147a70
Stephen Smithstone <stephen@smithstone.co.uk>
2012-08-07 07:24:18 +0100

    initial cucumber integration for swing version of scoobydoo

---
+++ src/test/java/scoobydoo/WeHaveSmokeStepdefs.java
@@ -0,0 +1,23 @@
+package scoobydoo;
+
+import cucumber.annotation.en.Given;
+import cucumber.annotation.en.Then;
+import cucumber.annotation.en.When;
+
+public class WeHaveSmokeStepdefs {
+
+    @Given("our application is running")
+    public void ourApplicationIsRunning(){
+
+    }
+
+    @When("i access the application status")
+    public void whatsTheStatus(){
+
+    }
+
+    @Then("i should get back a valid response")
+    public void shouldHaveValidResponse(){
+
+    }
+}
